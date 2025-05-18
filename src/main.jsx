4cb9b818ed962ae1b5d127e7f12164d4163eeac3
Suraj Thapa <suraj15025@gmail.com>
2025-05-18 06:42:24 +0000

    update in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { HashRouter } from 'react-router-dom';  // changed
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/Blog'>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+);
